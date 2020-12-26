abf03714e49d848c42ede9b1e9feff1b2a66432f
defauth98 <neto.daniribeiro@gmail.com>
2020-12-26 17:39:17 -0300

    Componente sidebar

---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+
+import { BsFillInboxFill, BsCalendar } from 'react-icons/bs';
+import { IoIosToday, IoIosDoneAll } from 'react-icons/io';
+
+const Header: React.FC = () => {
+  return (
+    <aside className="w-1/5 p-8 border-r-2">
+      <ul>
+        <li className="flex items-center my-4 cursor-pointer">
+          <BsFillInboxFill size={30} color="blue" />
+          <span className="px-4 text-lg text-blue-800 font-bold">Inbox</span>
+        </li>
+        <li className="flex items-center my-4 cursor-pointer">
+          <IoIosToday size={30} color="gray" />
+          <span className="px-4 text-lg text-gray-700">Today</span>
+        </li>
+        <li className="flex items-center my-4 cursor-pointer">
+          <BsCalendar size={30} color="gray" />
+          <span className="px-4 text-lg text-gray-700">Upcoming</span>
+        </li>
+        <li className="flex items-center my-4 cursor-pointer">
+          <IoIosDoneAll size={30} color="gray" />
+          <span className="px-4 text-lg text-gray-700">Completed</span>
+        </li>
+      </ul>
+    </aside>
+  );
+};
+
+export default Header;
--- src/pages/TodoListPage.tsx
+++ src/pages/TodoListPage.tsx
@@ -1,11 +1,19 @@
 import React from 'react';
+
 import Header from '../components/Header';
+import Sidebar from '../components/Sidebar';
 
 const TodoListPage: React.FC = () => {
   return (
-    <div className="flex bg-blue-500 max-w-full h-screen">
-      <div className="bg-white h-4/5 w-4/5 m-auto rounded-2xl">
-        <Header />
+    <div className="flex bg-blue-500 max-w-full h-screen ">
+      <div className="bg-white h-4/5 w-4/5 m-auto rounded-2xl overflow-hidden">
+        <div>
+          <Header />
+        </div>
+        <div className="flex h-full">
+          <Sidebar />
+          <main className="w-4/5">Tasks</main>
+        </div>
       </div>
     </div>
   );
